d2e9514dfa8963f38d783097e258b60e063e3919
akrem <akrem@NIKO>
2023-07-02 21:49:26 +0400

    Add project files.

---
+++ WebApplication1/Extension/MapWhenHakuna.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.Extension
+{
+    public class MapWhenHakuna
+    {
+        private RequestDelegate next;
+
+        public MapWhenHakuna(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            Console.WriteLine("start");
+            await next.Invoke(context);
+            Console.WriteLine("end");
+        }
+    }
+}
